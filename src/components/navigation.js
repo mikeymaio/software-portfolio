b0e39477d3560b144d179d55822c35560d4dac7d
mikeymaio <michael.maio22@gmail.com>
2020-01-07 20:44:38 -0500

    cleanup

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -10,27 +10,34 @@ if (typeof window !== "undefined") {
   })
 }
 
-export default () => {
+export default (props) => {
   const [menuOpen, setMenuOpen] = useState(false)
-
-  // const [isTop, setIsTop] = useState(window.scrollY < window.innerHeight - 61);
-  const [isTop, setIsTop] = useState(null);
+  const [isScrolled, setIsScrolled] = useState(null);
 
   useEffect(() => {
+    scrollOnLoad();
     handleScroll();
-    // setIsTop(window.scrollY < window.innerHeight - 61)
+
     document.addEventListener('scroll', handleScroll);
 
     return () => document.removeEventListener('scroll', handleScroll);
   }, [])
 
+  function scrollOnLoad() {
+    if (window.location.hash) {
+      const scrollDestination = document.getElementById(window.location.hash.split('#')[1]).getBoundingClientRect().top
+
+      return window.scrollTo({
+        top: scrollDestination,
+        behavior: 'smooth'
+      });
+    }
+  }
+
   function handleScroll() {
-    const top = window.scrollY < window.innerHeight - 61;
-    console.log('top: ', top);
-    if (top !== isTop) {
-      console.log('isTop: ', isTop);
-      console.log('top !== isTop');
-      setIsTop(top);
+    const scrolled = window.scrollY > window.innerHeight - 61;
+    if (scrolled !== isScrolled) {
+      setIsScrolled(scrolled);
     }
   }
 
@@ -42,81 +49,61 @@ export default () => {
 
   const burgerIconClassNames = [styles.navIcon, menuOpen ? styles.navIconOpen : ''].join(' ')
 
-  const navStyles = [styles.navContainer, !!isTop ? styles.navTop : ''].join(' ');
+  const navStyles = [styles.navContainer, !isScrolled ? styles.navTop : ''].join(' ');
 
-  console.log('navStyles: ', navStyles);
-
-return (
-  <nav role="navigation" className={navStyles}>
-    <div className={styles.mobileContainer}>
-    <button onClick={toggleMenu} className={burgerIconClassNames}>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-    <h1 className={styles.navTitle}>Michael Maio</h1>
-    </div>
-    <ul className={mobileNavClassNames}>
-      <li className={styles.navigationItem} key="home">
-        <Link to="/#home" onClick={toggleMenu}>Home</Link>
-      </li>
-      <li className={styles.navigationItem} key="about">
-        <Link to="/#about" onClick={toggleMenu}>About</Link>
-      </li>
-      <li className={styles.navigationItem} key="recentWork">
-        <Link to="/#recentWork" onClick={toggleMenu}>Recent Work</Link>
-      </li>
-      <li className={styles.navigationItem} key="clients">
-        <Link to="/#clients" onClick={toggleMenu}>Clients</Link>
-      </li>
-      <li className={styles.navigationItem} key="skills">
-        <Link to="/#skills" onClick={toggleMenu}>Skills</Link>
-      </li>
-      <li className={styles.navigationItem} key="contact">
-        <Link to="/#contact" onClick={toggleMenu}>Contact</Link>
-      </li>
-    </ul>
-    <ul className={styles.navigation}>
-      <Fade left cascade duration={1500}>
-      <li className={styles.navigationItem} key="home">
-        <Link to="/#home">Home</Link>
-      </li>
-      <li className={styles.navigationItem} key="about">
-        <Link to="/#about">About</Link>
-      </li>
-      <li className={styles.navigationItem} key="recentWork">
-        <Link to="/#recentWork">Recent Work</Link>
-      </li>
-      <li className={styles.navigationItem} key="clients">
-        <Link to="/#clients">Clients</Link>
-      </li>
-      <li className={styles.navigationItem} key="skills">
-        <Link to="/#skills">Skills</Link>
-      </li>
-      <li className={styles.navigationItem} key="contact">
-        <Link to="/#contact">Contact</Link>
-      </li>
-      </Fade>
-    </ul>
-  </nav>
-//   <div className={styles.nav}>
-//   <input type="checkbox" id="navCheck" className={styles.navCheck} />
-//   <div className={styles.navBtn}>
-//     <label htmlFor="navCheck">
-//       <span></span>
-//       <span></span>
-//       <span></span>
-//     </label>
-//   </div>
-//   <div className={styles.navLinks}>
-//     <Link to="#home">Home</Link>
-//     <Link to="#about">About</Link>
-//     <Link to="#recentWork">Recent Work</Link>
-//     <Link to="#clients">Clients</Link>
-//     <Link to="#skills">Skills</Link>
-//     <Link to="#contact">Contact</Link>
-//   </div>
-// </div>
-)
+  return (
+    <nav role="navigation" className={navStyles}>
+      <div className={styles.mobileContainer}>
+      <button onClick={toggleMenu} className={burgerIconClassNames}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <h1 className={styles.navTitle}>Michael Maio</h1>
+      </div>
+      <ul className={mobileNavClassNames}>
+        <li className={styles.navigationItem} key="home">
+          <Link to="/#home" onClick={toggleMenu}>Home</Link>
+        </li>
+        <li className={styles.navigationItem} key="about">
+          <Link to="/#about" onClick={toggleMenu}>About</Link>
+        </li>
+        <li className={styles.navigationItem} key="recentWork">
+          <Link to="/#recentWork" onClick={toggleMenu}>Recent Work</Link>
+        </li>
+        <li className={styles.navigationItem} key="clients">
+          <Link to="/#clients" onClick={toggleMenu}>Clients</Link>
+        </li>
+        <li className={styles.navigationItem} key="skills">
+          <Link to="/#skills" onClick={toggleMenu}>Skills</Link>
+        </li>
+        <li className={styles.navigationItem} key="contact">
+          <Link to="/#contact" onClick={toggleMenu}>Contact</Link>
+        </li>
+      </ul>
+      <ul className={styles.navigation}>
+        <Fade left cascade duration={1500}>
+        <li className={styles.navigationItem} key="home">
+          <Link to="/#home">Home</Link>
+        </li>
+        <li className={styles.navigationItem} key="about">
+          <Link to="/#about">About</Link>
+        </li>
+        <li className={styles.navigationItem} key="recentWork">
+          <Link to="/#recentWork">Recent Work</Link>
+        </li>
+        <li className={styles.navigationItem} key="clients">
+          <Link to="/#clients">Clients</Link>
+        </li>
+        <li className={styles.navigationItem} key="skills">
+          <Link to="/#skills">Skills</Link>
+        </li>
+        <li className={styles.navigationItem} key="contact">
+          <Link to="/#contact">Contact</Link>
+        </li>
+        </Fade>
+      </ul>
+    </nav>
+  )
 }
