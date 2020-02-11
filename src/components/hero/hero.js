18d42e201687d5ad786eb382774da200183bd9d0
mikeymaio <michael.maio22@gmail.com>
2020-02-11 17:42:33 -0500

    fix: gatsby build

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -5,24 +5,28 @@ import Fade from 'react-reveal/Fade';
 
 import styles from './hero.module.css'
 
-export default ({ data }) => (
-  <div className={styles.hero} id="home">
-    <Img
-      className={styles.heroImage}
-      alt={data.name}
-      fluid={data.heroImage.fluid}
-    />
-    <div className={styles.heroDetails}>
-      <h1 className={styles.heroHeadline}>
-        <Zoom left cascade={window.matchMedia('(min-width: 768px)').matches} duration={2000}>
-          {data.title}
-        </Zoom>
-      </h1>
-      <h2>
-        <Zoom right cascade={window.matchMedia('(min-width: 768px)').matches} duration={2500}>
-          {data.shortBio.shortBio}
-        </Zoom>
-      </h2>
+export default ({ data }) => {
+  const shouldCascade = typeof window !== 'undefined' && window ? window.matchMedia('(min-width: 768px)').matches : false;
+
+  return (
+    <div className={styles.hero} id="home">
+      <Img
+        className={styles.heroImage}
+        alt={data.name}
+        fluid={data.heroImage.fluid}
+      />
+      <div className={styles.heroDetails}>
+        <h1 className={styles.heroHeadline}>
+          <Zoom left cascade={shouldCascade} duration={2000}>
+            {data.title}
+          </Zoom>
+        </h1>
+        <h2>
+          <Zoom right cascade={shouldCascade} duration={2500}>
+            {data.shortBio.shortBio}
+          </Zoom>
+        </h2>
+      </div>
     </div>
-  </div>
-)
+  )
+}
