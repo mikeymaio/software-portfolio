342bfd9cdf68bae49120fba416e66c5392c63a3e
mikeymaio <michael.maio22@gmail.com>
2020-02-17 16:59:53 -0500

    add link overlay

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 import styles from './project.module.css'
 import Chip from '../chip/chip'
+import LinkOverlay from './project.overlay'
 
 export default ({ project }) => {
+  const [showLinks, setShowLinks] = useState(false)
+
   function renderCodeLink() {
-    if (!project.codeLink) return null;
+    if (!project.codeLink) return null
 
     if (project.codeLink.toLowerCase() === 'private') {
       return (
         <div className={styles.disabledLink}>
-          <i className={[styles.linkIcon, "fab fa-github"].join(' ')}></i>
+          <i className={[styles.linkIcon, 'fab fa-github'].join(' ')}></i>
           <small className={styles.tooltiptext}>code is private</small>
         </div>
       )
@@ -20,46 +23,75 @@ export default ({ project }) => {
 
     return (
       <a href={project.codeLink} target="_blank" className={styles.gitHub}>
-        <i className={[styles.linkIcon, "fab fa-github"].join(' ')}></i>
+        <i className={[styles.linkIcon, 'fab fa-github'].join(' ')}></i>
       </a>
     )
   }
 
+  function renderImage() {
+    if (!project.image) return null
+
+    return (
+      <div onClick={() => setShowLinks(true)}>
+        <Img fluid={project.image.fluid} alt={project.projectName} />
+      </div>
+    )
+  }
+
   return (
-  <div className={styles.project}>
-    {project.image && (
-      <Img fluid={project.image.fluid} alt={project.projectName} />
-    )}
-    <div className={styles.projectContent}>
-      <h3 className={styles.title}>
-        {`${project.company} - ${project.projectName}`}
-      </h3>
-      <p>{project.description}</p>
-      <h4 className={styles.linkContainerHeader}>Links:</h4>
-      <div className={styles.linkContainer}>
-        {project.webLink && project.webLink !== '0' && (
-          <a href={project.webLink} target="_blank" className={styles.chromeIcon}>
-            <i className={[styles.linkIcon, "fab fa-chrome"].join(' ')}></i>
-          </a>
-        )}
-        {project.iosLink && project.iosLink !== '0' && (
-          <a href={project.iosLink} target="_blank" className={styles.appStore}>
-            <i className={[styles.linkIcon, "fab fa-app-store"].join(' ')} ></i>
-          </a>
-        )}
-        {project.androidLink && project.androidLink !== '0' && (
-          <a href={project.androidLink} target="_blank" className={styles.android}>
-            <i className={[styles.linkIcon, "fab fa-android"].join(' ')}></i>
-          </a>
-        )}
-        {renderCodeLink()}
+    <div className={styles.project}>
+      {renderImage()}
+      <div className={styles.projectContent}>
+        <h3 className={styles.title}>
+          {`${project.company} - ${project.projectName}`}
+        </h3>
+        <p>{project.description}</p>
+        <h4 className={styles.linkContainerHeader}>Links:</h4>
+        <div className={styles.linkContainer}>
+          {project.webLink && project.webLink !== '0' && (
+            <a
+              href={project.webLink}
+              target="_blank"
+              className={styles.chromeIcon}
+            >
+              <i className={[styles.linkIcon, 'fab fa-chrome'].join(' ')}></i>
+            </a>
+          )}
+          {project.iosLink && project.iosLink !== '0' && (
+            <a
+              href={project.iosLink}
+              target="_blank"
+              className={styles.appStore}
+            >
+              <i
+                className={[styles.linkIcon, 'fab fa-app-store'].join(' ')}
+              ></i>
+            </a>
+          )}
+          {project.androidLink && project.androidLink !== '0' && (
+            <a
+              href={project.androidLink}
+              target="_blank"
+              className={styles.android}
+            >
+              <i className={[styles.linkIcon, 'fab fa-android'].join(' ')}></i>
+            </a>
+          )}
+          {renderCodeLink()}
+        </div>
+        <div className={styles.chipContainer}>
+          {project.technologies &&
+            project.technologies.map(tag => <Chip chip={tag} key={tag} />)}
+        </div>
       </div>
-      <div className={styles.chipContainer}>
-        {project.technologies && project.technologies.map(tag => (
-          <Chip chip={tag} key={tag} />
-        ))}
-      </div>
+      <LinkOverlay
+        showLinks={showLinks}
+        webLink={project.webLink}
+        iosLink={project.iosLink}
+        androidLink={project.androidLink}
+        codeLink={project.codeLink}
+        onClose={() => setShowLinks(false)}
+      />
     </div>
-  </div>
-)
+  )
 }
---
+++ src/components/project/project.overlay.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import styles from './project.overlay.module.css'
+
+export default ({
+  showLinks,
+  webLink,
+  iosLink,
+  androidLink,
+  codeLink,
+  onClose,
+}) => {
+  if (!showLinks) return null
+
+  return (
+    <div className={styles.linkOverlay}>
+      <div className={styles.overlayHeader}>
+        <div style={{ flex: 1 }}></div>
+        <h2 className={styles.overlayHeaderTitle}>Links</h2>
+        <button className={styles.closeOverlayButton} onClick={onClose}>
+          <i
+            className={[styles.closeOverlayIcon, 'fas fa-times'].join(' ')}
+          ></i>
+        </button>
+      </div>
+      <div className={styles.linkContainerColumn}>
+        {webLink && webLink !== '0' && (
+          <a href={webLink} target="_blank">
+            <h2>Web</h2>
+          </a>
+        )}
+        {iosLink && iosLink !== '0' && (
+          <a href={iosLink} target="_blank">
+            <h2>iOS</h2>
+          </a>
+        )}
+        {androidLink && androidLink !== '0' && (
+          <a href={androidLink} target="_blank">
+            <h2>Android</h2>
+          </a>
+        )}
+        {codeLink && codeLink.toLowerCase() !== 'private' && (
+          <a href={codeLink} target="_blank">
+            <h2>Code</h2>
+          </a>
+        )}
+      </div>
+    </div>
+  )
+}
